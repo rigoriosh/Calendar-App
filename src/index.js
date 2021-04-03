bbce3ae2a56815ed89f58eaebd5e1c0ee15b6213
rigoriosh <rigoriosh@gmail.com>
2021-04-03 12:29:06 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { CalendarApp } from './CalendarApp';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
